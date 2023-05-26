a04e6e4da9e0be7202eb867f9bee71a877e41121
yuijiro473 <yuichiro.aoki@asia-quest.jp>
2023-05-27 01:26:12 +0900

    回答

--- fixed_flight_booking.py
+++ fixed_flight_booking.py
@@ -1,41 +1,34 @@
+# fixed_flight_booking.py
+
 class Flight:
-    # 目的地と乗客リストをセット。引数がない場合はデフォルト値を設定
-    def __init__(self, destination='Default', passengers=[]): 
+    def __init__(self, destination='Default', passengers=None):
         self.destination = destination
-        self.passengers = passengers
+        self.passengers = passengers if passengers is not None else []
 
-    # 指定した乗客をフライトの乗客リストに追加
     def add_passenger(self, passenger):
-        passenger += self.passenger
+        self.passengers.append(passenger)
 
-    # フライトの乗客数を返す
     def passenger_count(self):
-        print(len(self.passengers))
+        return len(self.passengers)
 
-    # 乗客リストの最初の乗客を返す
     def get_first_passenger(self):
-        return self.passengers[0]
+        return self.passengers[0] if self.passengers else None
 
-    # 指定した乗客をフライトの乗客リストから削除
     def remove_passenger(self, passenger):
-        self.passengers.remove(passenger)
+        if passenger in self.passengers:
+            self.passengers.remove(passenger)
 
-    # フライトの目的地を新しい目的地に変更
     def change_destination(self, new_destination):
-        return new_destination
+        self.destination = new_destination
 
-    # フライトの乗客リストをソート
     def sort_passengers(self):
-        pass
+        self.passengers.sort()
 
-    # フライトの乗客リストを返す
     def list_passengers(self):
-        print(self)
+        return self.passengers
 
-    # 指定した乗客がフライトの乗客リストに含まれているか確認
     def is_passenger_on_flight(self, passenger):
-        return passenger
+        return passenger in self.passengers
 
-    # フライトの乗客リストを全てクリア
     def clear_all_passengers(self):
-        return
+        self.passengers.clear()
